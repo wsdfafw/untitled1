3f0560d4b8cae359552adbbabd3830fe8776a4e7
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-08-26 14:06:24 +0800

    测试

---
+++ src/main/java/github/buiawpkgew1/untitled1/Untitled1.java
@@ -0,0 +1,17 @@
+package github.buiawpkgew1.untitled1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Untitled1 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
